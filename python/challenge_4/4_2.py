1bf0b237845fdc167b7ddee04e934ab85248645a
Thomas Steck <thomas.steck@stretch.se>
2019-12-29 15:44:56 +0100

    challenge 4 part 2

--- python/challenge_4/4_2.py
+++ python/challenge_4/4_2.py
@@ -2,13 +2,14 @@ answer = 0
 
 for pwd in range(372304, 847060+1):
     digits = [int(x) for x in str(pwd)]
-    has_pair = any([digits[i] == digits[i+1] for i in range(len(digits)-1)])
+    has_pair = any([
+        (i == 0 or digits[i] != digits[i-1]) and
+        digits[i] == digits[i+1] and
+        (i == len(digits)-2 or digits[i] != digits[i+2]) for i in range(len(digits)-1)])
     never_decreases = any([digits[i] > digits[i+1]
                            for i in range(len(digits)-1)])
 
     if has_pair and not never_decreases:
         answer += 1
-
-    #print(digits, has_pair, never_decreases)
 
 print(answer)
